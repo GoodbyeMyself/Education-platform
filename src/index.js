eb0946f1bb0ac331ceb766e71371098cb2557b27
DESMGVHMNGQHF\Administrator <15500409521@163.com>
2017-11-22 11:31:12 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dva from 'dva';
+import 'moment/locale/zh-cn';
+import './polyfill';
+import './g2';
+import './raven';
+// import browserHistory from 'history/createBrowserHistory';
+import './index.less';
+
+// 1. Initialize
+const app = dva({
+  // history: browserHistory(),
+});
+
+// 2. Plugins
+// app.use({});
+
+// 3. Register global model
+app.model(require('./models/global'));
+
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
